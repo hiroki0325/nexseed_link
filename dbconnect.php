8af725dcf4427d84a853b7fa2474dbd093bcc450
hiroki0325 <nexseed.shimada@gmail.com>
2015-11-19 22:38:02 +0800

    .htaccess追加、DB情報修正

--- dbconnect.php
+++ dbconnect.php
@@ -1,4 +1,4 @@
 <?php
-    $db = mysqli_connect('localhost','root','mysql','nsfw') or die(mysqli_connect_error());
+    $db = mysqli_connect('localhost','root','mysql','nexseed_link') or die(mysqli_connect_error());
     mysqli_set_charset($db, 'utf8');
 ?>
