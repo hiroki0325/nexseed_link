f8659f64d84a492291cc4752d33744e5a573bbaa
Natsuki <runrun904@gmail.com>
2015-11-19 22:18:48 +0800

    ファンクションキーの追加

--- function.php
+++ function.php
@@ -53,4 +53,68 @@
         }
         return $plural;
     }
+
+    //htmlspecialchars
+    function h($value){
+      return htmlspecialchars($value,ENT_QUOTES,'UTF-8');
+    }
+
+
+    //ログイン判定
+    function isLoginSuccess(){
+
+      if(isset($_SESSION['id'])&& $_SESSION['time']+3600>time()){
+        //ログインしている
+        return true;
+      }else{
+        //ログインしていない
+        return false;
+      }
+    }
+
+
+    //ステータス判定
+    function status(){
+      $db=mysqli_connect('localhost','root','mysql','nexseed_link')or die(mysqli_connect_error());
+      mysqli_set_charset($db,'utf-8');
+
+      $sql=sprintf('SELECT*FROM users WHERE id=%d',
+          mysqli_real_escape_string($db,$_SESSION['id'])
+          );
+      var_dump($sql);
+      $record=mysqli_query($db,$sql)or die(mysqli_error($db));
+      $user=mysqli_fetch_assoc($record);
+      //来学予定者
+      if($user['status']=='future_student'){
+            return "future";
+      //在学生
+      }elseif($user['status']=='stay_student'){
+            return "stay";
+      //卒業生
+      }else{
+            return "graduate";
+      }
+    }
+      //各ページで機能をステータスによって出し分けたい時
+      //例：online英会話は卒業生以外に表示させない・使わせないなど
+
+
+    //ログアウト
+    function logout(){
+       $_SESSION=$arrayName = array();
+       if(ini_get("session.use_cokkie_params")){
+         $params=session_get_cookie_params();
+         setcookie(session_name(),'', time() - 42000,
+           $params["path"],$params["domain"],
+           $params["secure"],$params["httponly"]
+           );
+       }
+       session_destroy();
+
+       setcookie('email','',time()-3600);
+       setcookie('password','',time()-3600);
+
+       header('Location: login.php');
+       exit();
+    }
 ?>
