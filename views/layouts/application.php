0f3b9294fba145b39b571e92d05b13a1c71f9998
kadehina <geta206@gmail.com>
2015-12-09 10:35:51 +0800

    サイトレイアウト完成 #14

--- views/layouts/application.php
+++ views/layouts/application.php
@@ -7,28 +7,17 @@
   <meta name="viewport" content="width=device-width, initial-scale=1" />
 
   <link rel="stylesheet" href="../views/assets/css/style.css">
-
   <link rel="stylesheet" href="../views/assets/css/mypage/main.css" />
-  <link rel="stylesheet" href="../views/assets/css/mypage/timeline.css" />
   <link rel="stylesheet" href="../views/assets/css/bootstrap.css">
   <link rel="stylesheet" href="../views/assets/css/mypage/owl.carousel.css" />
   <link rel="stylesheet" href="../views/assets/css/mypage/magnific-popup.css" />
   <link rel="stylesheet" href="../views/assets/css/mypage/body.css" />
   <link rel="stylesheet" href="../views/assets/css/mypage/responsive.css" />
-
   <link rel="stylesheet" href="../views/assets/font-awesome/css/font-awesome.css">
 
-  <!-- Favicon -->
-  <link rel="shortcut icon" href="images/icon/favicon.png">
-  <link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/icon/apple-touch-icon-144-precomposed.png">
-  <link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/icon/apple-touch-icon-114-precomposed.png">
-  <link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/icon/apple-touch-icon-72-precomposed.png">
-  <link rel="apple-touch-icon-precomposed" href="images/icon/apple-touch-icon-57-precomposed.png">
 
 </head>
 <body>
-  <!-- <h1>NexSeedLink</h1> -->
-
   <!-- Header -->
   <div id="header">
 
@@ -37,7 +26,7 @@
       <div id="logo">
         <span class="image avatar48"><img src="images/avatar.jpg" alt="" /></span>
         <h1 id="title">Natuko</h1>
-        <p>natuko@gmail.com</p>
+        <p>hogeID</p>
       </div>
 
       <!-- Nav -->
@@ -46,7 +35,7 @@
           <li><a href="#top" id="top-link" class="skel-layers-ignoreHref"><span class="icon fa-home">ホーム</span></a></li>
           <li><a href="#portfolio" id="portfolio-link" class="skel-layers-ignoreHref"><span class="icon fa-th">英会話</span></a></li>
           <li><a href="#about" id="about-link" class="skel-layers-ignoreHref"><span class="icon fa-user">物流</span></a></li>
-          <li><a href="#contact" id="contact-link" class="skel-layers-ignoreHref"><span class="icon fa-envelope">詳細ページ</span></a></li>
+          <li><a href="#contact" id="contact-link" class="skel-layers-ignoreHref"><span class="icon fa fa-cog">詳細ページ</span></a></li>
         </ul>
       </nav>
     </div>
@@ -56,53 +45,28 @@
       <ul class="icons">
         <li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
         <li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
-        <li><a href="#" class="icon fa-github"><span class="label">Github</span></a></li>
-        <li><a href="#" class="icon fa-dribbble"><span class="label">Dribbble</span></a></li>
-        <li><a href="#" class="icon fa-envelope"><span class="label">Email</span></a></li>
+        <li><a href="#" class="icon fa fa-envelope"><span class="label">Email</span></a></li>
       </ul>
     </div>
   </div>
 
   <!-- Main -->
   <div id="main">
-
-
     <?php
-    $favicon =1;
+
         if (!isset($page)) {
             include('./views/' . $function . '/' . $directry . '.php');
         } else {
             include('./views/' . $function . '/' . $directry .  '/' . $page . '.php');
         }
      ?>
-
-
-    <!-- Scripts -->
-    <script src="../views/assets/js/jquery.min.js"></script>
-    <script src="../views/assets/js/mypage/jquery.scrolly.min.js"></script>
-    <script src="../views/assets/js/mypage/jquery.scrollzer.min.js"></script>
-    <script src="../views/assets/js/mypage/skel.min.js"></script>
-    <script src="../views/assets/js/mypage/util.js"></script>
-
-    <script src="../views/assets/js/bootstrap.js"></script><!-- Bootstrap -->
-    <script src="../views/assets/js/mypage/jquery.parallax.js"></script><!-- Parallax -->
-    <script src="../views/assets/js/mypage/smoothscroll.js"></script><!-- Smooth Scroll -->
-    <script src="../views/assets/js/mypage/masonry.pkgd.min.js"></script><!-- masonry -->
-    <script src="../views/assets/js/mypage/jquery.fitvids.js"></script><!-- fitvids -->
-    <script src="../views/assets/js/mypage/owl.carousel.min.js"></script><!-- Owl-Carousel -->
-    <script src="../views/assets/js/mypage/jquery.counterup.min.js"></script><!-- CounterUp -->
-    <script src="../views/assets/js/mypage/jquery.isotope.min.js"></script><!-- isotope -->
-    <script src="../views/assets/js/mypage/jquery.magnific-popup.min.js"></script><!-- magnific-popup -->
-    <script src="../views/assets/js/mypage/scripts.js"></script><!-- Scripts -->
-    <script src="../views/assets/js/mypage/main.js"></script>
-
-    <!-- /TimeLine -->
   </div><!-- /main -->
 
   <!-- FOOTER -->
   <footer id="footer">
     <!-- SOCIAL ICONS -->
     <div class="footer-social-icons">
+    </div>
     
     <!-- /SOCIAL ICONS -->
     <div class="copyright">
@@ -110,6 +74,24 @@
     </div>
   </footer>
   <!-- /FOOTER -->
+
+  <!-- Scripts -->
+  <script src="../views/assets/js/jquery.min.js"></script>
+  <script src="../views/assets/js/mypage/jquery.scrolly.min.js"></script>
+  <script src="../views/assets/js/mypage/jquery.scrollzer.min.js"></script>
+  <script src="../views/assets/js/mypage/skel.min.js"></script>
+  <script src="../views/assets/js/mypage/util.js"></script>
+  <script src="../views/assets/js/bootstrap.js"></script>
+  <script src="../views/assets/js/mypage/jquery.parallax.js"></script>
+  <script src="../views/assets/js/mypage/smoothscroll.js"></script>
+  <script src="../views/assets/js/mypage/masonry.pkgd.min.js"></script>
+  <script src="../views/assets/js/mypage/jquery.fitvids.js"></script>
+  <script src="../views/assets/js/mypage/owl.carousel.min.js"></script>
+  <script src="../views/assets/js/mypage/jquery.counterup.min.js"></script>
+  <script src="../views/assets/js/mypage/jquery.isotope.min.js"></script>
+  <script src="../views/assets/js/mypage/jquery.magnific-popup.min.js"></script>
+  <script src="../views/assets/js/mypage/scripts.js"></script>
+  <script src="../views/assets/js/mypage/main.js"></script>
 
 </body>
 </html>
--- views/member/mypage.php
+++ views/member/mypage.php
@@ -13,7 +13,7 @@
 								あなたも一つセブ島へ「お届け物」はじめてみませんか。</p>
 						</div>
 					</div>
-				</div>					
+				</div>
 			</div>
 
 			<div class="item" style="background-image: url(../views/assets/images/test.jpg)">
@@ -27,101 +27,10 @@
 								初心者の方でも大丈夫。まずは体験から</p>
 						</div>
 					</div>
-				</div>					
-			</div>
+				</div>
+			</div> <!-- /item -->
 			<a class="home-carousel-left" href="#home-carousel" data-slide="prev"><i class="fa fa-angle-left"></i></a>
 			<a class="home-carousel-right" href="#home-carousel" data-slide="next"><i class="fa fa-angle-right"></i></a>
-		</div>		
+		</div><!-- /carousel-inner -->
 	</div> <!--/#home-carousel--> 
-</section><!-- /SLIDER -->
-
-<!-- Time line -->
-<div id="timeline">
-
-	<div class="qa-message-list" id="wallmessages">
-
-		<div class="message-item" id="m16">
-			<div class="message-inner">
-				<div class="message-head clearfix">
-					<div class="avatar pull-left"><a href="./index.php?qa=user&qa_1=Oleg+Kolesnichenko"><img class="timeline" src="https://ssl.gstatic.com/accounts/ui/avatar_2x.png"></a></div>
-						<div class="user-detail">
-							<h5 class="handle">Oleg Kolesnichenko</h5>
-							<div class="post-meta">
-								<div class="asker-meta">
-									<span class="qa-message-what"></span>
-									<span class="qa-message-when">
-										<span class="qa-message-when-data">Jan 21</span>
-									</span>
-									<span class="qa-message-who">
-										<span class="qa-message-who-pad">by </span>
-										<span class="qa-message-who-data"><a href="./index.php?qa=user&qa_1=Oleg+Kolesnichenko">Oleg Kolesnichenko</a></span>
-									</span>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="qa-message-content">
-						Yo!
-					</div>
-				</div>
-			</div>
-		</div>
-
-		<div class="message-item" id="m7">
-			<div class="message-inner">
-				<div class="message-head clearfix">
-					<div class="avatar pull-left"><a href="./index.php?qa=user&qa_1=russell"><img class="timeline" src="https://ssl.gstatic.com/accounts/ui/avatar_2x.png"></a></div>
-						<div class="user-detail">
-							<h5 class="handle">russell</h5>
-							<div class="post-meta">
-								<div class="asker-meta">
-									<span class="qa-message-what"></span>
-									<span class="qa-message-when">
-										<span class="qa-message-when-data">Oct 25, 2013</span>
-									</span>
-									<span class="qa-message-who">
-										<span class="qa-message-who-pad">by </span>
-										<span class="qa-message-who-data"><a href="./index.php?qa=user&qa_1=russell">russell</a></span>
-									</span>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="qa-message-content">
-						Nullam porta leo vitae ipsum feugiat viverra. In sed placerat mi. Nullam euismod, quam in euismod rhoncus, tellus velit posuere tortor, non cursus nunc velit et lacus.
-					</div>
-				</div>
-			</div>
-
-		<div class="message-item" id="m7">
-			<div class="message-inner">
-				<div class="message-head clearfix">
-					<div class="avatar pull-left"><a href="./index.php?qa=user&qa_1=russell"><img class="timeline" src="https://ssl.gstatic.com/accounts/ui/avatar_2x.png"></a></div>
-						<div class="user-detail">
-							<h5 class="handle">russell</h5>
-							<div class="post-meta">
-								<div class="asker-meta">
-									<span class="qa-message-what"></span>
-									<span class="qa-message-when">
-										<span class="qa-message-when-data">Oct 25, 2013</span>
-									</span>
-									<span class="qa-message-who">
-										<span class="qa-message-who-pad">by </span>
-										<span class="qa-message-who-data"><a href="./index.php?qa=user&qa_1=russell">russell</a></span>
-									</span>
-								</div>
-							</div>
-						</div>
-		    	</div>
-	
-					<div class="qa-message-content">
-						Nullam porta leo vitae ipsum feugiat viverra. In sed placerat mi. Nullam euismod, quam in euismod rhoncus, tellus velit posuere tortor, non cursus nunc velit et lacus.
-					</div>
-			 </div>
-		 </div>
-	
-	</div><!-- timeline  -->
-
-	</div><!-- wallmessages -->
-
-</div><!-- /TimeLine -->
+</section><!-- /slider -->
