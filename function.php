1a6b0e904c8ae37e9c093822495374e4a4c36323
Natsuki <runrun904@gmail.com>
2015-12-11 16:15:25 +0800

    変更の取り消し

--- function.php
+++ function.php
@@ -76,7 +76,7 @@
     function status(){
         include('dbconnect.php');
         $sql = sprintf('SELECT*FROM users WHERE id=%d',
-            mysqli_real_escape_string($db,$_SESSION['join']['id'])
+            mysqli_real_escape_string($db,current_user('id')
         );
         $record = mysqli_query($db,$sql)or die(mysqli_error($db));
         $user = mysqli_fetch_assoc($record);
@@ -141,7 +141,7 @@
     function visit_log_time(){
         include('dbconnect.php');
         $sql=sprintf('UPDATE users SET visit_log_time=NOW() WHERE id=%d ',
-            mysqli_real_escape_string($db,$_SESSION['join']['id'])
+            mysqli_real_escape_string($db,currenet_user('id'))
             );
             mysqli_query($db, $sql) or die(mysqli_error($db));
     }
@@ -150,7 +150,7 @@
     function visit_log_time_show(){
         include('dbconnect.php');
         $sql = sprintf('SELECT*FROM users WHERE id=%d',
-            mysqli_real_escape_string($db,$_SESSION['join']['id'])
+            mysqli_real_escape_string($db,current_user('id'))
         );
         $record = mysqli_query($db,$sql)or die(mysqli_error($db));
         $user = mysqli_fetch_assoc($record);
