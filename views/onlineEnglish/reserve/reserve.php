0352f4b42a82759732e26428a302a1ba9becd243
Koichi <cx.w0077@gmail.com>
2015-12-23 11:18:54 +0800

    予約画面のCSS修正とSESSIONにニックネーム追加

--- views/onlineEnglish/reserve/reserve.php
+++ views/onlineEnglish/reserve/reserve.php
@@ -61,14 +61,11 @@
 <div class="container">
   <div class="row">
     <!-- メイン画面 -->
-    <div class="col-md-8">
+    <div class="media-row col-xs-12" style="margin-top:30px">
       <!-- お気に入り講師一覧のrow -->
       <div class="row title_k">
           <p>お気に入り講師から予約</p>
       </div>
-      <div class="row">
-          <div > <!-- class="col-md-4"を削除 -->
-            <div > 
                   <?php 
                       // 講師とレッスン時間が紐付いた配列を作成する
                       $like_teachers_lessons = array(); // すべてのお気に入りの先生のレッスン可能時間をいれる
@@ -114,20 +111,18 @@
                     }
                   ?>
                     
-            </div><!-- <div class="col-md-6"> -->
-
-           
-            <div >
-
-
                 <!-- お気に入り講師からの予約フォーム -->
                 
                   
                   <?php 
                       foreach ($data as $key1 => $value1) {
-                          echo '<form action="confirm" method="post">';
+                          echo '<div class= "col-sm-6 col-xs-12 col-md-4 col-lg-4">';
+                          echo '<div class="well">';
+                          
+                          
                           echo $key1;
                           echo "<br>";
+                          
                           
                           for ($i = 0; $i < count($value1); $i++) { 
                               $params = explode(' ', $value1[$i]);// データをスペース区切りで分解する
@@ -152,7 +147,9 @@
                               
 
                               //お気に入り講師の予約可能時間帯表示
-                              echo '<div class="box">';
+                              echo '<div class="media">';
+                              echo '<div class="media-body" >';
+                              echo '<form action="confirm" method="post">';
                               echo sprintf('<button class="btn btn-info" type="submit" name="lesson_id" value="%s">%s</button>',
                                           $lesson_id,
                                           // $datetime,
@@ -166,144 +163,100 @@
                                             $lesson_id,
                                             $lesson_id
                                             );
-                              // echo '</div>';
-                              // echo "</div>";
-                          echo '</form> ';
-                              
+                              echo '</form> ';
+                              echo '</div>';
+                              echo "</div>";
                           }
+                        echo '</div>';
+                        echo "</div>";
                       }
                   ?>
                   
-                
-            </div>
+    </div> <!-- media-row col-xs-12 bg-primary -->           
+  </div><!-- class="row" -->
 
-            <hr>
-            
-          </div><!-- <div class="col-md-4"> -->
-          <div class="col-md-4">
-            
-          </div>
-          <div class="col-md-4">
+ 
+  <!-- お気に入り講師終了 -->
+  <hr>
 
-          </div>
-      </div>
-      <!-- お気に入り講師終了 -->
-      <hr>
-
-      <!-- 日付指定用のrow -->
-      <div class="row">
-        <p class="title_k">日付を指定して予約</p>
-        <?php
-          if (isset($_REQUEST)) {
-             echo '<p>※<?php echo date("Y-m-d", strtotime($REQUEST));?></p>';
-           } 
-        ;?>
-        
-        <div class="col-md-12">
-          <ul>
-          <?php
-              for ($i=0; $i <=10 ; $i++) { 
-                echo sprintf("<li style=".'display:inline;'.">"."<a href='reserve?date=%s'>"."%s"."</a>"."</li>",
-                date("Y-m-d", strtotime("+$i day")),
-                date("n/j(D)", strtotime("+$i day"))
-                );
-              }
-              
-          ?>
-          </ul>
-        </div>
-      </div>
-      <!-- 日付指定終了 -->
-
-      <!-- 検索結果のためのrow -->
-      <div class="container">
-        <div class="row">
-          <?php while ($all_lesson = mysqli_fetch_assoc($all_lessons)) :?>
-
-              <div class= "col-sm-6 col-xs-12 col-md-4 col-lg-4">
-                <div class="well">
-                  <div class="media" >
-                    <!-- 講師の写真表示 -->
-                    <?php 
-                        if (isset($all_lesson["nickname"])) {
-                          echo sprintf('<img src="../../views/user/user_picture/%s" alt="画像" width="80" height="80" style="float:left"><p>%s先生</p>',
-                                        $all_lesson["picture"],
-                                        // "default.png",
-                                        $all_lesson["nickname"]
-                                        // "Daisy"
-                                      );
-                        }
-                        // お気に入り登録の重複確認
-                        $sql = sprintf('SELECT count(*) AS cnt FROM teacher_likes WHERE student_id=%d AND teacher_id=%d',
-                                        $_SESSION["join"]["id"],
-                                        $all_lesson["id"]
-                                        );
-                        $record = mysqli_query($db, $sql) or die(mysqli_error($db));
-                        $table = mysqli_fetch_assoc($record);
-                        if ($table["cnt"] > 0) {
-                            $error["like"] = "duplicate";
-                        }
-                        
-                    ?>
-
-                    <div class="media-body" >
-                      <!-- 予約可能時間帯の表示 -->
-                        <?php if (isset($all_lesson["lesson_id"])) :?>
-                            <form action="confirm" method="post">
-                              <input type="hidden" name="lesson_id" value="<?php echo $all_lesson["lesson_id"]; ?>" >
-                              <input type="hidden" name="nickname" value="<?php echo $all_lesson["nickname"]; ?>" >
-                              <input type="hidden" name="picture" value="<?php echo $all_lesson["picture"]; ?>" >
-                              <input type="hidden" name="time" value="<?php echo $all_lesson["time"]; ?>" >
-                              <button type="submit" name="date" value="<?php echo $all_lesson["date"];?>"><?php echo date("H時i分",strtotime($all_lesson["time"])) ;?></button>
-                            </form>
-                            <?php if (empty($error["duplicate"])) :?>
-                                <!-- <form action="" method="post">
-                                  <input type="hidden" name="teacher_id" value="<?php echo $all_lesson["id"]; ?>" >
-                                  <input type="submit" name="like" value="お気に入り追加">
-                                </form> -->
-                            <?php else :?>
-                                <!-- <label>登録済み</label> -->
-                            <?php endif ;?>
-                        <?php endif ;?>
-                    </div>
-                  </div>
-                </div>
-              </div>
-          <?php endwhile; ?>
-        </div>
-      </div>
-
-
-
-
-
-
-
-      <!-- <div class="row">
-
-        
-            <div class="col-md-4">
-              <div class="col-md-6">
-                
-
-
-              </div>
-              <div class="col-md-6">
-
-                
-              </div>
-            </div><! <div class="col-md-4"> -->
-        
-
-
-      <!-- </div> --><!-- <div class="row"> -->
-      <!-- 検索結果終了 --> -->
-    </div>
-    <!-- メイン画面終了 -->
-
-    <!-- サイドバー -->
-    <div class="col-md-4">
-      
+  <!-- 日付指定用のrow -->
+  <div class="row">
+    <p class="title_k">日付を指定して予約</p>
+    <?php
+      if (isset($_REQUEST)) {
+         echo '<p>※<?php echo date("Y-m-d", strtotime($REQUEST));?></p>';
+       } 
+    ;?>
+    
+    <div class="col-md-12">
+      <ul>
+      <?php
+          for ($i=0; $i <=10 ; $i++) { 
+            echo sprintf("<li style=".'display:inline;'.">"."<a href='reserve?date=%s'>"."%s"."</a>"."</li>",
+            date("Y-m-d", strtotime("+$i day")),
+            date("n/j(D)", strtotime("+$i day"))
+            );
+          }
+          
+      ?>
+      </ul>
     </div>
   </div>
+  <!-- 日付指定終了 -->
+
+  <!-- 検索結果のためのrow -->
+    <div class="row">
+      <?php while ($all_lesson = mysqli_fetch_assoc($all_lessons)) :?>
+
+          <div class= "col-sm-6 col-xs-12 col-md-6 col-lg-4">
+            <div class="well">
+              <div class="media" >
+                <!-- 講師の写真表示 -->
+                <?php 
+                    if (isset($all_lesson["nickname"])) {
+                      echo sprintf('<img src="../../views/user/user_picture/%s" alt="画像" width="80" height="80" style="float:left"><p>%s先生</p>',
+                                    $all_lesson["picture"],
+                                    // "default.png",
+                                    $all_lesson["nickname"]
+                                    // "Daisy"
+                                  );
+                    }
+                    // お気に入り登録の重複確認
+                    $sql = sprintf('SELECT count(*) AS cnt FROM teacher_likes WHERE student_id=%d AND teacher_id=%d',
+                                    $_SESSION["join"]["id"],
+                                    $all_lesson["id"]
+                                    );
+                    $record = mysqli_query($db, $sql) or die(mysqli_error($db));
+                    $table = mysqli_fetch_assoc($record);
+                    if ($table["cnt"] > 0) {
+                        $error["like"] = "duplicate";
+                    }
+                    
+                ?>
+
+                <div class="media-body" >
+                  <!-- 予約可能時間帯の表示 -->
+                    <?php if (isset($all_lesson["lesson_id"])) :?>
+                        <form action="confirm" method="post">
+                          <input type="hidden" name="lesson_id" value="<?php echo $all_lesson["lesson_id"]; ?>" >
+                          <input type="hidden" name="nickname" value="<?php echo $all_lesson["nickname"]; ?>" >
+                          <input type="hidden" name="picture" value="<?php echo $all_lesson["picture"]; ?>" >
+                          <input type="hidden" name="time" value="<?php echo $all_lesson["time"]; ?>" >
+                          <button type="submit" name="date" value="<?php echo $all_lesson["date"];?>"><?php echo date("H時i分",strtotime($all_lesson["time"])) ;?></button>
+                        </form>
+                        <?php if (empty($error["duplicate"])) :?>
+                            <!-- <form action="" method="post">
+                              <input type="hidden" name="teacher_id" value="<?php echo $all_lesson["id"]; ?>" >
+                              <input type="submit" name="like" value="お気に入り追加">
+                            </form> -->
+                        <?php else :?>
+                            <!-- <label>登録済み</label> -->
+                        <?php endif ;?>
+                    <?php endif ;?>
+                </div>
+              </div>
+            </div>
+          </div>
+      <?php endwhile; ?>
+    </div>
 </div>
